63cd4d853d1742e0721e10b92683c1f5b685d183
leffw <93302536+leffw@users.noreply.github.com>
2023-07-16 21:36:34 -0300

    refactor: lnurl withdraw

--- src/interfaces/api/__init__.py
+++ src/interfaces/api/__init__.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from src.services.hygraph import hygraph
 from src.services.lndhub import lndhub
 from src.interfaces.api import schemas
@@ -5,12 +6,21 @@ from fastapi.responses import JSONResponse
 from src.configs import API_HOST, API_PORT, API_DNS
 from fastapi import FastAPI, HTTPException, Request
 from src.lib import lnurl
+from base64 import b64decode
 from src import database
 
+import logging
 import uvicorn
 
 # Initialize API.
 api = FastAPI(docs_url=None, redoc_url=None, openapi_url=None, swagger_ui_oauth2_redirect_url=None)
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @api.get("/health/liveness")
 async def health_liveness():
@@ -30,29 +40,30 @@ def lnurlw_details(user_id: str, voucher: str, request: Request):
         return JSONResponse({
             "status": "ERROR", 
             "reason": "Does not exist"
-        }, status_code=204)
+        }, status_code=400)
     else:
         reward = reward.get()
 
     return {
         "tag": "withdrawRequest",
-        "callback": API_DNS + f"/withdraw/api/v1/lnurl/{user_id}/w",
+        "callback": API_DNS + f"/withdraw/api/v1/lnurlw/{user_id}",
         "k1": voucher, 
         "minWithdrawable": reward.value * 1000, 
         "maxWithdrawable": reward.value * 1000, 
     }
 
-@api.get("/withdraw/api/v1/lnurl/{user_id}/w")
+@api.get("/withdraw/api/v1/lnurlw/{user_id}")
 def lnurlw_execute(user_id: str, k1: str = None, pr: str = None):
     reward = database.Reward.select().where(
         (database.Reward.user == user_id) & 
         (database.Reward.id == k1) & 
         (database.Reward.status == "created"))
     if not reward.exists():
+        logging.error("Reward does not exist")
         return JSONResponse({
             "status": "ERROR", 
             "reason": "Does not exist"
-        }, status_code=204)
+        }, status_code=400)
     else:
         reward = reward.get()
     
@@ -64,7 +75,8 @@ def lnurlw_execute(user_id: str, k1: str = None, pr: str = None):
             "reason": "Invoice is invalid."
         }, status_code=400)
     
-    invoice_amount = int(decode_invoice["amount_msat"] / 1000)
+    print(decode_invoice)
+    invoice_amount = decode_invoice["num_satoshis"]
     if invoice_amount != reward.value:
         return JSONResponse({
             "status": "ERROR", 
@@ -74,7 +86,11 @@ def lnurlw_execute(user_id: str, k1: str = None, pr: str = None):
     reward.status = "pending"
     reward.save()
     
-    payinvoice = lndhub.payinvoice(pr)
+    try:    
+        payinvoice = lndhub.payinvoice(pr)
+    except:
+        payinvoice = {}
+    
     if not payinvoice.get("payment_preimage"):
         reward.status = "created"
         reward.save()        
@@ -90,16 +106,24 @@ def lnurlw_execute(user_id: str, k1: str = None, pr: str = None):
 @api.post("/api/v1/reward/{classroom}")
 def reward(classroom: str, data: schemas.QuizSchema, request: Request):
     email = request.headers.get("X-EMAIL")
-    if not email:
+    if not email or email == "null":
         raise HTTPException(400, "Invalid email address.")
-
-    user_id = database.User.get_or_create(email=email)[0].id
-    if database.Reward.select().where(
-        (database.Reward.user == user_id) & 
-        (database.Reward.classroom == classroom)).exists():
-        raise HTTPException(409, "The prize claim has been created.") 
     
-    answers = list(set(data.answers))
+    user_id = database.User.get_or_create(email=email)[0].id
+    reward = database.Reward.select(database.Reward.id).where(
+        (database.Reward.user == user_id) & 
+        (database.Reward.classroom == classroom))
+    if reward.exists():
+        reward = reward.get().id
+        lnurlw = lnurl.lnurl_encode(f"{API_DNS}/withdraw/api/v1/lnurl/{user_id}/{reward}")
+        return JSONResponse({ "lnurl": lnurlw })
+    
+    try:
+        answers = b64decode(data.answers).decode("utf-8")
+    except:
+        answers = b64decode(data.answers).decode("latin-1")
+    
+    answers = list(set(answers.split("&")))
     quizzes = hygraph.call(r'''
         {
           quizzes(where: {classroom: "%s"}){
@@ -131,11 +155,9 @@ def reward(classroom: str, data: schemas.QuizSchema, request: Request):
         status="created",
         classroom=classroom
     )[0].id
-    return JSONResponse({
-        "lnurl": lnurl.lnurl_encode(
-            API_DNS + f"/withdraw/api/v1/lnurl/{user_id}/{reward}"
-        )
-    }, status_code=201)
+    
+    lnurlw = lnurl.lnurl_encode(f"{API_DNS}/withdraw/api/v1/lnurl/{user_id}/{reward}")
+    return JSONResponse({ "lnurl": lnurlw })
 
 def start():
     uvicorn.run(api, host=API_HOST, port=API_PORT, loop="asyncio", log_config={
--- src/interfaces/api/schemas.py
+++ src/interfaces/api/schemas.py
@@ -1,4 +1,4 @@
 from pydantic import BaseModel, EmailStr
 
 class QuizSchema(BaseModel):
-    answers: list
+    answers: str
